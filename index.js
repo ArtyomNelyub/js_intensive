de3541856a0aa2e466802766cd4e90af1a4213d5
Artyom Nelyub <artyom.nelyub.an@gmail.com>
2022-08-25 09:59:20 +0400

    Realized first task

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log('Hi');
+function changeNumberSystem() {
+  let number = +prompt('Введите число: ');
+  let numberSystem = +prompt('Введите систему счисления: ');
+
+  if (
+    !(
+      Number.isFinite(number) &&
+      Number.isFinite(numberSystem) &&
+      numberSystem >= 2 &&
+      numberSystem <= 36
+    )
+  ) {
+    console.log('Некорректный ввод');
+    return;
+  }
+
+  console.log(
+    `${number} в системе счисления ${numberSystem}: ${number.toString(
+      numberSystem
+    )}`
+  );
+}
+
+changeNumberSystem();
