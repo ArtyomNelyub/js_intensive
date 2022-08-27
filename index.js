c4248d29015007e5bb66cb0a53ca2130f280de48
Artyom Nelyub <artyom.nelyub.an@gmail.com>
2022-08-27 19:48:53 +0400

    Realised function makeObjectDeepCopy

--- index.js
+++ index.js
@@ -23,4 +23,46 @@ function calculation() {
   );
 }
 
-calculation();
+function makeObjectDeepCopy(entity) {
+  let result;
+
+  if (Array.isArray(entity)) {
+    result = makeArrayCopy(entity);
+  } else {
+    result = makeObjectCopy(entity);
+  }
+
+  function makeArrayCopy(arr) {
+    let gap = [];
+
+    for (let i = 0; i < arr.length; i++) {
+      if (Array.isArray(arr[i])) {
+        gap[i] = makeArrayCopy(arr[i]);
+      } else if (!Array.isArray(arr[i]) && typeof arr[i] === 'object') {
+        gap[i] = makeObjectCopy(arr[i]);
+      } else {
+        gap[i] = arr[i];
+      }
+    }
+
+    return gap;
+  }
+
+  function makeObjectCopy(obj) {
+    let gap = {};
+
+    for (let key in obj) {
+      if (Array.isArray(obj[key])) {
+        gap[key] = makeArrayCopy(obj[key]);
+      } else if (!Array.isArray(obj[key]) && typeof obj[key] === 'object') {
+        gap[key] = makeObjectCopy(obj[key]);
+      } else {
+        gap[key] = obj[key];
+      }
+    }
+
+    return gap;
+  }
+
+  return result;
+}
